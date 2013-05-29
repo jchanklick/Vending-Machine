dda00e4ccbbfce6ad71cbdf47f8795b1c1ec2e5f
unknown <cdahl@cdahl.klick.com>
2013-05-29 12:44:49 -0400

    Project setup for windows services

---
+++ WindowsApp/ProjectKService/ProjectKService/ProjectKService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectKService
+{
+    using System;
+    using System.IO;
+
+
+    // from: http://www.codeproject.com/Articles/106742/Creating-a-simple-Windows-Service
+    class ProjectKService : System.ServiceProcess.ServiceBase
+    {
+        // The main entry point for the process
+        static void Main()
+        {
+            System.ServiceProcess.ServiceBase[] ServicesToRun;
+            ServicesToRun =
+              new System.ServiceProcess.ServiceBase[] { new ProjectKService() };
+            System.ServiceProcess.ServiceBase.Run(ServicesToRun);
+        }
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ServiceName = "ProjectKService";
+        }
+        private string folderPath = @"c:\temp";
+        /// <summary>
+        /// Set things in motion so your service can do its work.
+        /// </summary>
+        protected override void OnStart(string[] args)
+        {
+            if (!System.IO.Directory.Exists(folderPath))
+                System.IO.Directory.CreateDirectory(folderPath);
+
+            FileStream fs = new FileStream(folderPath + "\\WindowsService.txt",
+                                FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter m_streamWriter = new StreamWriter(fs);
+            m_streamWriter.BaseStream.Seek(0, SeekOrigin.End);
+            m_streamWriter.WriteLine(" WindowsService: Service Started at " +
+               DateTime.Now.ToShortDateString() + " " +
+               DateTime.Now.ToShortTimeString() + "\n");
+            m_streamWriter.Flush();
+            m_streamWriter.Close();
+        }
+        /// <summary>
+        /// Stop this service.
+        /// </summary>
+        protected override void OnStop()
+        {
+            FileStream fs = new FileStream(folderPath +
+              "\\WindowsService.txt",
+              FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter m_streamWriter = new StreamWriter(fs);
+            m_streamWriter.BaseStream.Seek(0, SeekOrigin.End);
+            m_streamWriter.WriteLine(" WindowsService: Service Stopped at " +
+              DateTime.Now.ToShortDateString() + " " +
+              DateTime.Now.ToShortTimeString() + "\n");
+            m_streamWriter.Flush();
+            m_streamWriter.Close();
+        }
+    }
+}
---
+++ WindowsApp/ProjectKService/ProjectKService/ServiceInstaller.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectKService
+{
+    // from: http://www.codeproject.com/Articles/106742/Creating-a-simple-Windows-Service
+    [System.ComponentModel.RunInstaller(true)]
+    public class ProjectInstaller : System.Configuration.Install.Installer
+    {   
+        private System.ServiceProcess.ServiceInstaller serviceInstaller;
+        private System.ServiceProcess.ServiceProcessInstaller 
+                serviceProcessInstaller;
+
+        public ProjectInstaller()
+        {
+            // This call is required by the Designer.
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.serviceInstaller = new System.ServiceProcess.ServiceInstaller();
+            this.serviceProcessInstaller = 
+              new System.ServiceProcess.ServiceProcessInstaller();
+            // 
+            // serviceInstaller
+            // 
+            this.serviceInstaller.Description = "My Windows Service description";
+            this.serviceInstaller.DisplayName = "Project K Service";
+            this.serviceInstaller.ServiceName = "ProjectKService";
+            // 
+            // serviceProcessInstaller
+            // 
+            this.serviceProcessInstaller.Account = 
+              System.ServiceProcess.ServiceAccount.LocalService;
+            this.serviceProcessInstaller.Password = null;
+            this.serviceProcessInstaller.Username = null;
+            // 
+            // ServiceInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller,
+            this.serviceInstaller});
+
+        }
+    }
+}
